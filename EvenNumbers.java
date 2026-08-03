7510c38e97b9750f2d12298208e4159e3ec80d9f
Sara <sarajpanackal2029@cs.sjcetpalai.ac.in>
2026-08-03 23:17:36 +0530

    Create EvenNumbers.java

---
+++ EvenNumbers.java
@@ -0,0 +1,9 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int i = 2;
+        do {
+            System.out.print(i + " ");
+            i += 2;
+        } while (i <= 20);
+    }
+}
